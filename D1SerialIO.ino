656a7876b8117782ea9df8cf6a03a31cb57146a7
@jcassel <jcassel@systemwarden.com>
2023-02-18 10:30:52 -0500

    update for stroage and retreval of IO config

--- D1SerialIO.ino
+++ D1SerialIO.ino
@@ -1,22 +1,20 @@
 //simple D1 mini Serial IO Driver
-//Takes serial commands and configures IO. 
 //Takes serial commands to control configured IO.
-//IO can have interupt (send info to serial) on trigger
-//Can create automations for interactions between inputs, outputs and actions. 
+//Can create automations for interactions between inputs, outputs and actions. (Future)
 
-
-//Should support a closed loop temerature control. Need to have a sensor and a output to control a heating element. Could also do a fan / exaust system to cool enclosure down. 
+//Eventually should support a closed loop temerature control. Need to have a sensor and a output to control a heating element. Could also do a fan / exaust system to cool enclosure down. 
 
 //Target outputs 
 //Digital/relay: PSU,Lighting,Enclosure heater, alarm, filter relay,exaust fan
-//Digital (PWM):cooling fan(PWM*)
+//Digital (PWM):cooling fan(PWM*) (Future)
 
 //Target inputs
 //Digital(5v/3.3v): General switches(NO or NC Selectable)
-//temperature inputs(TBA on type)
+//temperature inputs(Future + TBA on type)
 
-#define VERSIONINFO "D1SerialIO 1.0.0"
-#define COMPATIBILITY "PSUControlSerialPlugin 0.1.0"
+
+#define VERSIONINFO "D1SerialIO 1.0.3"
+#define COMPATIBILITY "SIOPlugin 0.1.1"
 #include "TimeRelease.h"
 #include <ArduinoJson.h> 
 #include "FS.h"
@@ -46,6 +44,10 @@ Bounce Bnc[IOSize];
 bool EventTriggeringEnabled = 1;
 
 
+
+bool isOutPut(int IOP){
+  return IOType[IOP] == OUTPUT; 
+}
 
 void ConfigIO(){
   if(loadIOConfig()){
@@ -98,8 +100,6 @@ void loop() {
   if(!_pauseReporting){
     reportIO(checkInputs());
   }
-  
-  
 }
 
 
@@ -112,7 +112,7 @@ void reportIO(bool forceReport){
       }
       Serial.print(IO[i]);
     }
-    Serial.print(analogRead(A0));
+    //Serial.print(analogRead(A0)); //future maybe
     Serial.println();
     IOReport.set(reportInterval);
     
@@ -165,12 +165,12 @@ void checkSerial(){
     }
     
     if(command == "BIO"){ 
-      Serial.println("OK");
+      ack();
       _pauseReporting = false; //restarts IO reporting 
       
     }    
     else if(command == "EIO"){ //this is the command meant to test for good connection.
-      Serial.println("OK");
+      ack();
       _pauseReporting = true; //stops all IO reporting so it does not get in the way of a good confimable response.
       //Serial.print("Version ");
       //Serial.println(VERSIONINFO);
@@ -178,40 +178,70 @@ void checkSerial(){
       //Serial.println(COMPATIBILITY);
       
     }
-    else if (command == "help"){
-      Serial.println("To set an IO point by index 1-7 Use the following. a=index; b=[0](low) or [1](high) ");
-      Serial.println("IO [a]:[b]");
-      Serial.println("To adjust io report timmings use the following");
-      Serial.println("SI [n]");
-      Serial.println("[n] is a range from 500 to 30000");
-      Serial.println("To turn on event reporting use the following:");
-      Serial.println("OK");
-    }
-    else if(command =="debug"){if(value == "1"){_debug = true;}else{_debug=false;}
-    }
 
-    else if(command=="IOT"){reportIOTypes();}
+    
+    else if (command == "IC") { //io count.
+      ack();
+      Serial.print("IC:");
+      Serial.println(IOSize);
+    }
+        
+    else if(command =="debug"){
+      ack();
+      if(value == "1"){
+        _debug = true;
+        Serial.println("Serial debug On");
+      }else{
+        _debug=false;
+        Serial.println("Serial debug Off");
+      }
+    }
+    else if(command=="CIO"){ //set IO Configuration
+      ack();
+      if (validateNewIOConfig(value)){
+        updateIOConfig(value);
+      }
+    }
+    
+    else if(command=="SIO"){
+      ack();
+      if(!saveIOConfig()){
+        Serial.println("Failed to save config.");
+      }
+    }
+    else if(command=="IOT"){
+      ack();
+      reportIOTypes();
+    }
     
     //Set IO point high or low (only applies to IO set to output)
     else if(command =="IO" && value.length() > 0){
-      int IOPoint = value.substring(0,1).toInt();
+      ack();
+      int IOPoint = value.substring(0,value.indexOf(" ")).toInt();
       int IOSet = value.substring(value.indexOf(" ")+1).toInt();
       if(_debug){
         Serial.print("IO #:");
         Serial.println(IOMap[IOPoint]);
         Serial.print("Set:");Serial.println(IOSet);
       }
-      if(IOSet == 1){digitalWrite(IOMap[IOPoint],HIGH);}else{digitalWrite(IOMap[IOPoint],LOW);}
-      reportIO(true);
-      Serial.println("OK");   
+      if(isOutPut(IOPoint)){
+        if(IOSet == 1){
+          digitalWrite(IOMap[IOPoint],HIGH);
+        }else{
+          digitalWrite(IOMap[IOPoint],LOW);
+        }
+      }else{
+        Serial.println("ERROR: Attempt to set IO which is not an output");   
+      }
+        reportIO(true);
     }
     
     //Set AutoReporting Interval  
     else if(command =="SI" && value.length() > 0){
+      ack();
       unsigned long newTime = value.toInt(); //will convert to a full long.
       if(newTime >=500){
         reportInterval = newTime;
-        Serial.println("OK");
       }else{
         Serial.println("ERROR: Value to small min 500ms");
       }
@@ -219,18 +249,53 @@ void checkSerial(){
 
     //Enable event trigger reporting Mostly used for E-Stop
     else if(command == "SE" && value.length() > 0){
+      ack();
       EventTriggeringEnabled = value.toInt();
-      Serial.println("OK");
     }
 
     //Get States 
     else if(command == "GS"){
+      ack();
       reportIO(true);
-      Serial.println("OK");
     }
+    else{
+      Serial.print("ERROR: Unrecognized command[");
+      Serial.print(command);
+      Serial.println("]");
+    }
+  }
+}
 
-    else{Serial.print("ERROR: Unrecognized command[");Serial.print(command);Serial.println("]");}
-  
+void ack(){
+  Serial.println("OK");
+}
+
+bool validateNewIOConfig(String ioConfig){
+  if(ioConfig.length() != IOSize){
+    return false;  
+  }
+
+  for (int i=0;i<IOSize;i++){
+    int pointType = ioConfig.substring(i,i+1).toInt();
+    if(pointType > 4){//cant be negative. we would have a bad parse on the number
+      if(_debug){
+        Serial.print("Bad IO Point type: index[");Serial.print(i);Serial.print("] type[");Serial.print(pointType);Serial.println("]");
+      }
+      return false;
+    }
+  }
+  return true; //seems its a good set of point Types.
+}
+
+void updateIOConfig(String newIOConfig){
+  for (int i=2;i<IOSize;i++){//start at 2 to avoid D0 and D1
+    int nIOC = newIOConfig.substring(i,i+1).toInt();
+    if(IOType[i] != nIOC){
+      IOType[i] = nIOC;
+      if(nIOC == OUTPUT){
+        digitalWrite(IOMap[i],LOW); //set outputs to low since they will be high coming from INPUT_PULLUP
+      }
+    }
   }
 }
 
@@ -241,6 +306,8 @@ int getIOType(String typeName){
   if(typeName == "INPUT_PULLDOWN"){return 3;}
   if(typeName == "OUTPUT_OPEN_DRAIN"){return 4;} //not sure on this value have to double check
 }
+
+
 bool loadIOConfig(){
   
 if (!SPIFFS.exists("/IOConfig.json"))
@@ -276,6 +343,46 @@ if (!SPIFFS.exists("/IOConfig.json"))
 
   return true;
 }
+
+
+bool saveIOConfig(){
+  SPIFFS.remove("/IOConfig.json");
+  File file = SPIFFS.open("/IOConfig.json", "w");
+
+  DynamicJsonDocument doc(256);
+
+  JsonObject configObj = doc.to<JsonObject>();
+  
+  configObj["IO0"]= getIOTypeString(IOType[0]);
+  configObj["IO1"]= IOType[1];
+  configObj["IO2"]= IOType[2];
+  configObj["IO3"]= IOType[3];
+  configObj["IO4"]= IOType[4];
+  configObj["IO5"]= IOType[5];
+  configObj["IO6"]= IOType[6];
+  configObj["IO7"]= IOType[7];
+  configObj["IO8"]= IOType[8];
+    
+  if (serializeJsonPretty(doc, file) == 0)
+  {
+    Serial.println("[WARNING]: Failed to write to file:/config/mqttconfig.json");
+    return false;
+  }
+  file.close();
+  
+  return true;
+}
+
+
+
+String getIOTypeString(int ioType){
+  if (ioType = 0){return "INPUT";}
+  if (ioType = 1){return "OUTPUT";}
+  if (ioType = 2){return "INPUT_PULLUP";}
+  if (ioType = 3){return "INPUT_PULLDOWN";}
+  if (ioType = 4){return "OUTPUT_OPEN_DRAIN";}
+}
+
 
 
 
